47ae471d673fbcf2a60be7175ebc13003e077a03
JV-Amorim <joaovitoramorimx@gmail.com>
2023-11-05 14:27:12 -0300

    UserEntity class

---
+++ src/main/java/br/edu/ifnmg/gestaoprocessos/user/UserEntity.java
@@ -0,0 +1,64 @@
+package br.edu.ifnmg.gestaoprocessos.user;
+
+import br.edu.ifnmg.gestaoprocessos.base.BaseEntity;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+@Entity(name = "User")
+@Table(name = "users")
+public class UserEntity extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private UserRole role;
+    
+    @Column(nullable = false, length = 100)
+    private String name;
+    
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+    
+    @Column(nullable = false)
+    private String passwordHash;
+
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+    //</editor-fold>
+    
+}
---
+++ src/main/java/br/edu/ifnmg/gestaoprocessos/user/UserRole.java
@@ -0,0 +1,7 @@
+package br.edu.ifnmg.gestaoprocessos.user;
+
+public enum UserRole {
+    ADMIN,
+    EDITOR,
+    USER
+}
