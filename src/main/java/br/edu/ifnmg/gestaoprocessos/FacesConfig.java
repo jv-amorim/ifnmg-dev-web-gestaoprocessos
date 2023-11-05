f023c4852414da337b3b96a57c4367f7d4162a35
JV-Amorim <joaovitoramorimx@gmail.com>
2023-11-05 14:34:18 -0300

    FacesConfig bean adicionado

---
+++ src/main/java/br/edu/ifnmg/gestaoprocessos/FacesConfig.java
@@ -0,0 +1,9 @@
+package br.edu.ifnmg.gestaoprocessos;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@javax.faces.annotation.FacesConfig(version = javax.faces.annotation.FacesConfig.Version.JSF_2_3)
+public class FacesConfig {
+    
+}
